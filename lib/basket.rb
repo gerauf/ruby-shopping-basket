002bc8800e1f9afd0a9c0d06b6ec183212c134ba
Fergus Orbach <fo4786@gmail.com>
2016-07-20 19:16:08 +0100

    adds a basic total method

--- lib/basket.rb
+++ lib/basket.rb
@@ -2,10 +2,16 @@ class Basket
 
   def initialize product_catalogue
     @product_catalogue = product_catalogue
+    @orders = []
   end
 
   def add product_code
-    @product_catalogue.find { |product| product.code == product_code }
+    @orders << @product_catalogue.find { |product| product.code == product_code }
   end
+
+  def total
+    @orders.reduce(0){|total, order| total += order.price}
+  end
+
 
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -5,14 +5,21 @@ describe Basket do
   let(:blouse)  { double :blouse, code: 'B01', price: 24.95}
   let(:socks)   { double :socks, code: 'S01', price: 7.95}
   let(:product_catalogue) {[socks, jeans, blouse]}
+  # should have a list method which returns the products
   subject(:basket) { Basket.new product_catalogue}
 
   describe '#add' do
     it 'finds the product in the product catalogue' do
-      expect(basket.add 'J01').to eq jeans
+      expect(basket.add 'J01').to include jeans
     end
   end
 
-  
+  describe '#total' do
+    it 'adds up the value of all items in the basket' do
+      basket.add 'B01'
+      basket.add 'S01'
+      expect(basket.total).to eq 32.90
+    end
+  end
 
 end
