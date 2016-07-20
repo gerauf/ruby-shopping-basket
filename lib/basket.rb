e53e6dbc95bc0a9b020158d3ac3f77b8ccb6abb6
Fergus Orbach <fo4786@gmail.com>
2016-07-20 19:08:33 +0100

    add method added to basket

--- lib/basket.rb
+++ lib/basket.rb
@@ -1,3 +1,11 @@
 class Basket
 
+  def initialize product_catalogue
+    @product_catalogue = product_catalogue
+  end
+
+  def add product_code
+    @product_catalogue.find { |product| product.code == product_code }
+  end
+
 end
--- spec/basket_spec.rb
+++ spec/basket_spec.rb
@@ -1,8 +1,18 @@
 require 'basket'
 
 describe Basket do
-  subject(:basket) { Basket.new }
+  let(:jeans)   { double :jeans, code: 'J01', price: 32.95}
+  let(:blouse)  { double :blouse, code: 'B01', price: 24.95}
+  let(:socks)   { double :socks, code: 'S01', price: 7.95}
+  let(:product_catalogue) {[socks, jeans, blouse]}
+  subject(:basket) { Basket.new product_catalogue}
 
+  describe '#add' do
+    it 'finds the product in the product catalogue' do
+      expect(basket.add 'J01').to eq jeans
+    end
+  end
 
+  
 
 end
